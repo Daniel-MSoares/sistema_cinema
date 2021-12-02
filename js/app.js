235780fe4de5d279d46b84bcdbcafe86f4015742
Daniel-MSoares <munizsoaresrapper@gmail.com>
2021-12-01 21:36:52 -0300

    entidade filme concluída,proximo passo->sessões

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 class Filme{
     constructor(){
-        this.id=1
-        this.editar=null;
         this.lsFilmes=JSON.parse(localStorage.getItem('filmes'))
         this.lsFilmesDestaque=JSON.parse(localStorage.getItem('filmesDestaque'))
+        this.id=this.lsFilmes !== null ? (this.lsFilmes.length+1) : 1
+        this.editar=null;
+        
         this.filmes= this.lsFilmes !== null ? this.lsFilmes : []
         this.filmesDestaque= this.lsFilmesDestaque !== null ? this.lsFilmesDestaque : []
     }
@@ -11,23 +12,35 @@ class Filme{
         localStorage.setItem('filmes',JSON.stringify(this.filmes))
         localStorage.setItem('filmesDestaque',JSON.stringify(this.filmesDestaque))
     }
-    adicionar(){
-      let filme=this.lerDados();
-      if(filme.destaque){
+    salvar(){
+        let filme=this.lerDados();
+        if(this.validaCampos(filme)){
+             if(this.editar==null){
+                  this.adicionar(filme)
+             }else{
+                  this.atualizarFilme(this.editar,filme)
+                  
+            }
+        }    
+    }
+
+
+
+    adicionar(filme){
+      if(filme.destaque && this.filmesDestaque.length<=5){
          this.filmesDestaque.push(filme); 
       }
-
       this.filmes.push(filme);
       this.id++;
       this.atualizaLocalStorage();
       this.listarFilmesAdmin();
       this.limparCampos();
-      
-      
     }
-    
     verificaDestaques(){
-       
+        if(this.filmesDestaque.length>=5){
+            alert("Limite de filmes em detaque atingido(5)")
+            document.getElementById('destacar').checked=!document.getElementById('destacar').checked
+        }
     }
 
     lerDados(){
@@ -46,7 +59,8 @@ class Filme{
       objFilme.classificacao=document.getElementById('classificacaoFilme').value;
       objFilme.generoFilme=document.getElementById('generoFilme').value;
        
-      //recebe o caminho passado pelo input tipo file e extrai o nome da imagem que está na pasta img
+      //recebe o caminho passado pelo input tipo file e extrai o nome da imagem que está na pasta
+      console.log(document.getElementById('imgFilme').value)
       let textoCaminho=JSON.stringify(document.getElementById('imgFilme').value);
       textoCaminho='img/'+textoCaminho.slice(15)
       textoCaminho=textoCaminho.replace(/"/,'')
@@ -55,6 +69,39 @@ class Filme{
       return objFilme
     }
 
+
+    validaCampos(filme){
+        let msg=''
+        if(filme.nomeFilme===''){
+            msg+='-Informe o nome do filme- \n'
+        }
+        if(filme.sinopse===''){
+            msg+='-Informe uma sinopse do filme- \n'
+        }
+        if(filme.classificacao===''){
+            msg+='-selecione a classificação do filme- \n'
+        }
+        if(filme.generoFilme===''){
+            msg+='-selecione o genero do filme- \n'
+        }
+
+        if(filme.imgFilme==='img/'){
+           msg+='-selecione uma imagem para o filme- \n'
+        }
+        
+        
+
+        if(msg!==''){
+            alert(msg);
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+
     limparCampos(){
         document.getElementById('destacar').checked=false;
         document.getElementById('nomeFilme').value=''
@@ -62,8 +109,16 @@ class Filme{
         document.getElementById('classificacaoFilme').value=''
         document.getElementById('generoFilme').value=''
         document.getElementById('imgFilme').value=''
+        document.getElementById('btnSave').innerText='salvar';
+
+        document.getElementById('destacar').classList.remove('invisible');
+        document.getElementById('labelDestacar').classList.remove('invisible');
+        document.getElementById('labelImgFilme').innerText="Selecione a imagem do filme:"
+        document.getElementById('tituloForm').innerText='ADICIONAR NOVO TÍTULO';
         
-      }
+        this.editar=null
+    
+    }
     listarFilmesAdmin(){
         let listaFilmes=this.filmes
         let listaDestaques=this.filmesDestaque
@@ -80,7 +135,7 @@ class Filme{
             let td_id=tr.insertCell().innerText=filme.id;
             let td_nome=tr.insertCell().innerText=filme.nomeFilme;
             let td_acao=tr.insertCell();
-            td_acao.innerHTML=`<button onClick='filme.preparaEditar(${dadoFilme})' ><i class='fas fa-edit'></i>Editar</button>||
+            td_acao.innerHTML=`<button onClick='filme.preparaAtualizar(${dadoFilme})' ><i class='fas fa-edit'></i>Editar</button>||
             <button onClick='filme.excluirFilme(${filme.id})'> <i style='color:red;'class='fas fa-trash-alt'></i>Excluir</button> ||
             <button ><i class='fas fa-info-circle'></i><a href='ingresso.html?obj=${dadoFilme}'>detalhes</a></button>`
            });
@@ -114,13 +169,13 @@ class Filme{
                link.href='pages/ingresso.html?obj='+dadoFilme;
                link.innerHTML=`<img class="cartaz" src="${destaque.imgFilme}" alt=${destaque.nomeFilme}">`
                destaques.appendChild(link)
-               console.log(dadoFilme)
+               
         });
  
 
 //  lista todos
         
-               listaFilmes.forEach(filme => {
+            listaFilmes.forEach(filme => {
                let dadoFilme=JSON.stringify(filme)
                let link=document.createElement('a');
                link.href='pages/ingresso.html?obj='+dadoFilme;
@@ -151,16 +206,51 @@ class Filme{
     
     }
     
-    preparaEditar(dados){
+    preparaAtualizar(dados){
         this.editar=dados.id;
         console.log(dados)
         document.getElementById('nomeFilme').value=dados.nomeFilme;
-          
+        document.getElementById('btnSave').innerText='Atualizar';
+        document.getElementById('sinopseFilme').value=dados.sinopse;
+        document.getElementById('classificacaoFilme').value=dados.classificacao;
+        document.getElementById('generoFilme').value=dados.generoFilme;
+        document.getElementById('labelImgFilme').innerText="por favor,selecione a imagem novamente"
+
+        document.getElementById('destacar').classList.add('invisible');
+        document.getElementById('labelDestacar').classList.add('invisible');
+        document.getElementById('tituloForm').innerText='ATUALIZAR FILME';
     }
 
 
-    editarFilme(){
-
+    atualizarFilme(id,filmeAt){
+        for(let i =0;i<this.filmes.length;i++){
+            if(this.filmes[i].id===id){
+                this.filmes[i].nomeFilme=filmeAt.nomeFilme
+                this.filmes[i].sinopse=filmeAt.sinopse
+                this.filmes[i].generoFilme=filmeAt.generoFilme
+                this.filmes[i].classificacao=filmeAt.classificacao
+                this.filmes[i].imgFilme=filmeAt.imgFilme
+            }
+        }
+        this.atualizarDestaque(id,filmeAt)
+        this.listarFilmesAdmin();
+        this.atualizaLocalStorage();
+        this.limparCampos()
+        
+    }
+    atualizarDestaque(id,filmeAt){
+            for(let i =0;i<this.filmesDestaque.length;i++){
+                if(this.filmesDestaque[i].id===id){
+                    this.filmesDestaque[i].nomeFilme=filmeAt.nomeFilme
+                    this.filmesDestaque[i].sinopse=filmeAt.sinopse
+                    this.filmesDestaque[i].generoFilme=filmeAt.generoFilme
+                    this.filmesDestaque[i].classificacao=filmeAt.classificacao
+                    this.filmesDestaque[i].imgFilme=filmeAt.imgFilme
+                }
+                this.atualizaLocalStorage();
+                
+                this.limparCampos();  
+            }   
     }
 
     excluirFilme(id){
